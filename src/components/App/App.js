f97b61ba83b0c8ddea45bfcd2bb1702ce70a7a6b
Jakiepoo <joehersheyu45@gmail.com>
2020-11-01 21:20:29 -0800

    Added routes for other components to be rendered through the sidebar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
 import Home from "../Home/Home";
 import About from "../About/About";
+import Portfolio from "../Portfolio/Portfolio";
+import Contact from "../Contact/Contact";
 
 class App extends Component {
   render() {
@@ -9,6 +11,8 @@ class App extends Component {
       <Switch>
         <Route exact path="/" component={Home}/>
         <Route path="/about" component={About}/>
+        <Route path="/portfolio" component={Portfolio}/>
+        <Route path="/contact" component={Contact}/>
       </Switch>
     );
 }
