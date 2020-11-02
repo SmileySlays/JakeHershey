ac3eb6a15021d28028f237f1dc6125018d660a5a
Jakiepoo <joehersheyu45@gmail.com>
2020-11-02 02:15:54 -0800

    Updates

---
+++ precache-manifest.12638d450c85b497c0e2ce61a1725fb6.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f819d0182f2d3e819ed354e22b6b8bd4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "2922bb17af5e74696619",
+    "url": "/portfolio/static/css/2.6f0afc0c.chunk.css"
+  },
+  {
+    "revision": "67650b4f32e92e7fdd74",
+    "url": "/portfolio/static/css/main.85b36d74.chunk.css"
+  },
+  {
+    "revision": "2922bb17af5e74696619",
+    "url": "/portfolio/static/js/2.c7accede.chunk.js"
+  },
+  {
+    "revision": "27df78d2822439c2fc110c0a385025d0",
+    "url": "/portfolio/static/js/2.c7accede.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67650b4f32e92e7fdd74",
+    "url": "/portfolio/static/js/main.e96006fb.chunk.js"
+  },
+  {
+    "revision": "b417b3ee81e3e8f9aad5",
+    "url": "/portfolio/static/js/runtime-main.b649af6b.js"
+  },
+  {
+    "revision": "aad248f1020a0f27d7a343181f5dfdcf",
+    "url": "/portfolio/static/media/avery.aad248f1.jpeg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/portfolio/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/portfolio/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/portfolio/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/portfolio/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/portfolio/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/portfolio/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/portfolio/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/portfolio/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/portfolio/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/portfolio/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/portfolio/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "a1dc9b933f3636074bce852896330326",
+    "url": "/portfolio/static/media/logo.a1dc9b93.svg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/portfolio/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/portfolio/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/portfolio/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/portfolio/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/portfolio/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "5bc3cab7ceb614270d1a0466c270fddf",
+    "url": "/portfolio/static/media/portland-splash.5bc3cab7.jpg"
+  }
+]);
