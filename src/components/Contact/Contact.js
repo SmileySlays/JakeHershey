fbb31550673c99f7191b2c0499bb40eefcdc4348
Jakiepoo <joehersheyu45@gmail.com>
2020-11-01 21:23:59 -0800

    Added basic contact form (not complete)

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import {
+  Sidebar,
+  Grid,
+  GridRow,
+  Card,
+  Header,
+  Icon,
+  List,
+  Button,
+  Form
+} from "semantic-ui-react";
+import LeftSidebar from "../LeftSidebar/LeftSidebar";
+import Avery from "../../avery.jpeg";
+
+export default function Contact() {
+  return (
+    <>
+      <LeftSidebar />
+      <Sidebar.Pusher style={{ backgroundColor: "#141414" }}>
+        <Grid style={{ paddingBottom: "20%" }}>
+          <GridRow />
+          <GridRow columns={3}>
+            <Grid.Column width={3}></Grid.Column>
+            <Grid.Column textAlign="center" width={10}>
+              <Form>
+                <Form.Field>
+                  <label>First Name</label>
+                  <input placeholder="First Name" />
+                </Form.Field>
+                <Form.Field>
+                  <label>Last Name</label>
+                  <input placeholder="Last Name" />
+                </Form.Field>
+                <Button type="submit">Submit</Button>
+              </Form>
+            </Grid.Column>
+            <Grid.Column width={3}></Grid.Column>
+          </GridRow>
+        </Grid>
+      </Sidebar.Pusher>
+    </>
+  );
+}
